5304d8b4c1b9bda7709cf859db590e25d66d4f90
tarunkbyteiq-sketch <tarunk.byteiq@gmail.com>
2025-12-16 16:14:49 +0530

    Update prod.py

--- backend1/prod.py
+++ backend1/prod.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from .routes import router
 
-# app = FastAPI(title="Two-File FastAPI App")
+app = FastAPI(title="Two-File FastAPI App")
 
 # include routes from routes.py
 app.include_router(router)
